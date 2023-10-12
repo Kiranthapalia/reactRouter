f3c88dd4361df8be65aa2d87bb0f6f20d7fc9e6c
Kiranthapalia <thapaliakiran143@gmail.com>
2023-10-12 23:36:10 +0300

    Router dom created

---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import Home from './Home.jsx'
+import About from './About.jsx'
+import Contact from './Contact.jsx'
+import Error from './Error.jsx'
+import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+const router = createBrowserRouter([ // Import components that are used in routes
+{
+path: "/",
+element: <App />,
+errorElement: <Error />,
+children: [ // children are nested routes with a route
+{
+element: <Home />,
+index: true // index route does not need any path
+},
+{
+path: "about", // path can be defined relative to the parent path
+element: <About />,
+},
+{
+path: "contact",
+element: <Contact />,
+},
+]
+}
+]);
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <RouterProvider router={router} />
+  </React.StrictMode>,
+);
